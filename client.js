8e0f910f9aea6ce15f160bbce85a45bdba615b7c
lilyx13 <ashlyn@lilyx.io>
2023-10-06 11:56:33 -0600

    submit and clear button functionality added
    Signed-off-by: lilyx13 <ashlyn@lilyx.io>

--- client.js
+++ client.js
@@ -4,3 +4,19 @@ let userInput = document.querySelector("#input-box");
 const submitButton = document.querySelector("#submit-button");
 // output paragraph (show user content here)
 const displayUserInput = document.querySelector("#display-input");
+// clear button
+const clearButton = document.querySelector("#clear-button");
+
+// this will fire whenever the submit button is clicked
+submitButton.addEventListener("click", function () {
+  // update display user input
+  displayUserInput.textContent = userInput.value;
+  // remove content that is in the text box
+  userInput.value = "";
+});
+
+// whenever the clear button is clicked...
+clearButton.addEventListener("click", function () {
+  //  clear the displayUserInput box
+  displayUserInput.textContent = "";
+});
